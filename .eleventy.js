58bf4fbff4acef05b820e91a10a82bdc0ab923ab
Luigi Foscari <luigi.foscari@icloud.com>
2022-03-31 19:32:15 +0200

    removed _site, added assets

--- .eleventy.js
+++ .eleventy.js
@@ -4,5 +4,6 @@ module.exports = function (eleventyConfig) {
 
   eleventyConfig.addPassthroughCopy({
     './node_modules/alpinejs/dist/cdn.js': './js/alpine.js',
+    './assets': './assets'
   })
 };
